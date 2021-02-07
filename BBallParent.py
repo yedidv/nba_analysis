58aa059d4a4f4404e49854f979294c58f33a101b
yedidv <58367573+yedidv@users.noreply.github.com>
2021-02-07 17:51:29 -0500

    Initial commit

---
+++ BBallParent.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+#%%
+import pandas as pd 
+from selenium import webdriver
+
+class ParseURLS: 
+    def __init__(self, year): 
+        self.bball_url = 'https://www.basketball-reference.com/leagues/' 
+        self.year = year
+        self.hoopshype_salary_url = 'https://hoopshype.com/salaries/players/'
+
+    def ToDF(self, text, headers, th = None, hoopshype = False): 
+        ## Add the year column to the dataframe 
+        
+        df = pd.DataFrame(text, columns = headers) 
+        df['year'] = self.year 
+        if hoopshype is True: 
+            return df
+        elif th is not None: 
+            df['Th'] = th
+
+        return df
+    
+    def CleanURL(self, url, xpath): 
+        '''find the stats given the url and the xpath of the
+        table container'''
+        
+        browser = webdriver.Safari() 
+        browser.get(url) 
+        
+        team_stats = browser.find_element_by_xpath(xpath) 
+        
+        
+        
+        
+        ## Find all the headers 
+        headers = team_stats.find_elements_by_tag_name('thead')[0] 
+        headers = headers.text.split() 
+        
+        ## Find all the text 
+        body = team_stats.find_elements_by_tag_name('tbody')[0]
+        rows = body.find_elements_by_tag_name('tr') 
+        
+        ##Fill out all the rows 
+        text = [] 
+        th = []
+        for row in rows: 
+            elements = row.find_elements_by_tag_name('td') 
+            th_element = row.find_element_by_tag_name('th')
+            th.append(th_element.text)
+            element_text = [] 
+            for element in elements: 
+                element_text.append(element.text) 
+                
+            text.append(element_text) 
+        
+        browser.quit() 
+
+        return text, headers, th
+    
+
+
+    def Data(self, url, xpath, conference = False):
+
+
+        text, headers, th = self.CleanURL(url, xpath)
+        if conference is True: 
+            df = self.ToDF(text, headers[2:], th) 
+            df['playoffs'] = df.Th.apply(lambda x: 1 if '*' in x else 0) 
+            df.Th = df.Th.str.replace('*','')
+            return df
+        
+        df = self.ToDF(text, headers[1:]) 
+        return df    
+        
+    def HoopsHype(self, url, xpath): 
+        
+
+
+
+    
+        for j in range(10):
+            try:
+                
+                browser = webdriver.Safari()
+                browser.get(url) 
+                browser.implicitly_wait(5)
+                team_stats = browser.find_element_by_xpath(xpath) 
+              
+                    
+            except: 
+                print('{} hoops hype not found {}'.format(j, self.year))
+                print('trying again') 
+                browser.quit()
+                continue
+            if j == 9: 
+                print('hoops hype xpath not found {}. \nReturning "None"'.format(self.year))
+                return None
+            break
+            
+
+        headers = team_stats.find_element_by_tag_name('thead') 
+        headers = headers.text.split() 
+
+        body = team_stats.find_element_by_tag_name('tbody') 
+
+        rows = body.find_elements_by_tag_name('tr') 
+
+        content = [] 
+        for row in rows: 
+            row_text = row.text.split()[1:] 
+
+            while len(row_text) > 3: 
+                row_text[0:2] = [' '.join(row_text[0:2])]
+            
+            content.append(row_text) 
+        browser.quit() 
+
+        headers = ['Player', 'Salary', 'adjusted_salary']
+        player_salary = self.ToDF(
+            content, headers, hoopshype=True
+        )
+
+        
+
+        return player_salary
+
+    def FormatPlayerNames(self,players_series):
+        new_players_series = players_series.str.replace(
+            ' II', '').replace(
+                ' III', '').replace(
+                    ' Jr', '').replace(
+                        '.', '')
+        new_players_series = new_players_series.str.strip()
+        new_players_series = new_players_series.str.lower()
+
+        return players_series
+# %%
---
+++ bball_reference.py
@@ -0,0 +1,196 @@
+#%%
+from numpy.core.numeric import NaN
+import pandas as pd 
+from selenium import webdriver
+import numpy as np 
+from BBallParent import ParseURLS
+
+
+
+class BBallScrape(ParseURLS): 
+
+    import pandas as pd 
+    from selenium import webdriver
+    import numpy as np 
+    from BBallParent import ParseURLS
+   
+    def __init__(self, year): 
+       super().__init__(year)
+       
+    
+    def TeamStats(self): 
+        ## Create a dataframe to bring in 
+        ## the team data
+        
+        ## Read in the team url given the year
+        url = self.bball_url + 'NBA_{}.html'.format(
+            self.year)
+        team_xpath = '//*[@id="team-stats-per_game"]'
+        opponent_xpath = '//*[@id="opponent-stats-per_game"]'
+
+        team_stats = super().Data(
+            url,
+            team_xpath)
+        opponent_stats = super().Data(
+            url, 
+            opponent_xpath) 
+
+
+
+        return team_stats, opponent_stats
+
+
+    
+    
+    def Standings(self): 
+        ## See how many wins each team has against each other
+        url = self.bball_url + 'NBA_{}_standings.html'.format(self.year)
+        standings_xpath = '//*[@id="team_vs_team"]'
+        table_name = 'confs' if self.year >= 2016 else 'divs'
+        eastern_conference_xpath = '//*[@id="{}_standings_E"]'.format(
+            table_name)
+        western_conference_xpath = '//*[@id="{}_standings_W"]'.format(
+            table_name) 
+            
+        
+
+
+
+
+        east = super().Data(
+            url, 
+            eastern_conference_xpath,
+             conference = True) 
+        west = super().Data(
+            url, 
+            western_conference_xpath, 
+            conference = True) 
+        standings = super().Data(
+            url, 
+            standings_xpath) 
+
+        records = east.append(west) 
+
+        standings = standings.merge(
+            records, 
+            left_on = 'Team', right_on = 'Th', 
+            how = 'outer'
+        )
+
+        return standings
+    
+    def PlayerStats(self): 
+        url = self.bball_url + 'NBA_{}_per_game.html'.format(
+            self.year
+        )
+        players_xpath = '//*[@id="per_game_stats"]'
+
+        player_stats = super().Data(
+            url, players_xpath
+        )
+
+
+        
+        
+        
+        player_stats['Player'] = super().FormatPlayerNames(player_stats['Player']) 
+
+
+
+
+
+        return player_stats
+
+    def PlayerSalary(self, current_year = False):
+        if current_year is True: 
+            url = self.hoopshype_salary_url
+        else: 
+            url = self.hoopshype_salary_url + '{}-{}/'.format(
+                self.year - 1, self.year
+            )
+        
+        salary_xpath = '//*[@id="content-container"]/div/div[3]/div[2]/table'
+
+
+
+        player_salary = super().HoopsHype(
+            url, salary_xpath
+        )
+
+        player_salary['Player'] = super().FormatPlayerNames(player_salary['Player']) 
+
+        return player_salary
+    
+    def ReturnAllData(self): 
+
+        self.player_salary = self.PlayerSalary(
+            current_year = True if self.year is 2021 else False
+        )
+
+        self.player_stats = self.PlayerStats()
+        
+        self.standings = self.Standings() 
+        self.team_stats, self.opponent_stats = self.TeamStats() 
+        self.salary_cap = self.SalaryCap()
+
+        self.all_stats = {
+            'player_salary': self.player_salary, 
+            'player_stats': self.player_stats, 
+            'standings': self.standings, 
+            'team_stats': self.team_stats, 
+            'opponent_stats': self.opponent_stats, 
+            'salary_cap': self.salary_cap
+        }
+
+        return self.all_stats
+    
+    def SalaryCap(self): 
+
+        url = 'https://www.basketball-reference.com/contracts/salary-cap-history.html'
+
+
+        browser = webdriver.Safari() 
+        browser.get(url) 
+        browser.implicitly_wait(5) 
+        try: 
+            head_text = True
+            head = browser.find_element_by_xpath(
+            '//*[@id="salary_cap_history"]/thead'
+            )
+
+            body = browser.find_element_by_xpath(
+                    '//*[@id="salary_cap_history"]/tbody'
+            )
+            body = body.find_elements_by_tag_name('tr')
+
+
+
+        except: 
+            head_text = False
+            head = browser.find_element_by_xpath(
+            '//*[@id="salary_cap_history"]/tbody/tr[1]'
+            )
+            body = browser.find_element_by_xpath(
+            '//*[@id="salary_cap_history"]/tbody'
+            )
+            body = body.find_elements_by_tag_name('tr')[1:]
+        print(body[0].text)
+
+        table_body = []
+        for row in body: 
+            row_text = row.text.split('$')
+            table_body.append(row_text) 
+
+        head = [x.strip() for x in head.text.split('\n')]
+        head = [x for x in head if x]
+        print(head_text) 
+
+        self.salary_cap = pd.DataFrame(table_body, columns = head) 
+
+
+        browser.quit()
+
+        return self.salary_cap
+
+
+
